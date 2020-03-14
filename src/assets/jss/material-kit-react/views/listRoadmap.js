22d199da7987e0df6662ec2d0fa07986de2d019e
matheusrajeduzz <matheus.mendes@eduzz.com>
2020-03-14 12:47:10 -0300

    WIP

---
+++ src/assets/jss/material-kit-react/views/listRoadmap.js
@@ -0,0 +1,127 @@
+import { container, title } from 'assets/jss/material-kit-react.js';
+import imagesStyle from 'assets/jss/material-kit-react/imagesStyles.js';
+
+const listRoadmapStyle = {
+  container,
+  button_text: {
+    color: '#ffffff'
+  },
+  button: {
+    width: 'calc(100% - 16px)',
+    backgroundColor: '#99b6f2',
+    margin: '0 8px 12px 8px',
+    '&:hover': {
+      backgroundColor: '#466bb9'
+    }
+  },
+  profile: {
+    textAlign: 'center',
+    '& img': {
+      maxWidth: '160px',
+      width: '100%',
+      margin: '0 auto',
+      transform: 'translate3d(0, -50%, 0)'
+    }
+  },
+  description: {
+    margin: '1.071rem auto 0',
+    maxWidth: '600px',
+    color: '#999',
+    textAlign: 'center !important'
+  },
+  name: {
+    marginTop: '-80px'
+  },
+  ...imagesStyle,
+  main: {
+    background: '#FFFFFF',
+    position: 'relative',
+    zIndex: '3'
+  },
+  mainRaised: {
+    margin: '-60px 30px 0px',
+    borderRadius: '6px',
+    boxShadow: '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
+  },
+  title: {
+    ...title,
+    color: '#ffffff',
+    display: 'inline-block',
+    position: 'relative',
+    marginTop: '30px',
+    minHeight: '32px',
+    textDecoration: 'none'
+  },
+  socials: {
+    marginTop: '0',
+    width: '100%',
+    transform: 'none',
+    left: '0',
+    top: '0',
+    height: '100%',
+    lineHeight: '41px',
+    fontSize: '20px',
+    color: '#999'
+  },
+  navWrapper: {
+    margin: '20px auto 50px auto',
+    textAlign: 'center'
+  },
+  content: {
+    padding: 24
+  },
+  link: {
+    color: '#000000',
+    '&:hover': {
+      textDecoration: 'none',
+      cursor: 'pointer',
+      color: '#24229b'
+    }
+  },
+  roadrop: {
+    borderBottom: '1px solid #cecece',
+    textDecoration: 'none',
+    padding: 16,
+    '&:hover': {
+      transitionDuration: '0.2s',
+      transitionProperty: 'background-color',
+      backgroundColor: 'rgb(29,161,242,0.04)'
+    },
+
+    '& .roadrop__header': {
+      alignItems: 'baseline',
+      display: 'flex',
+      justifyContent: 'space-between',
+      textDecoration: 'none'
+    },
+    '& .roadrop__main': {
+      margin: '10px 0',
+      padding: '0 90px'
+    },
+
+    '& .roadrop__footer': {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: '0 32px',
+
+      '& .roadrop__honor': {
+        display: 'flex',
+        alignItems: 'center',
+
+        '& span:last-child': {
+          fontSize: 18,
+          marginLeft: 4
+        }
+      },
+
+      '& .roadrop__comment': {
+        display: 'flex',
+        alignItems: 'center',
+        margin: '4.5px 0'
+      }
+    }
+  }
+};
+
+export default listRoadmapStyle;
---
+++ src/pages/Roadmap/ListRoadmap.js
@@ -0,0 +1,104 @@
+import Icon from '@material-ui/core/Icon';
+import { makeStyles } from '@material-ui/core/styles';
+import CommentIcon from '@material-ui/icons/Comment';
+import Follow from '@material-ui/icons/EmojiPeople';
+import HonorIcon from '@material-ui/icons/Star';
+import styles from 'assets/jss/material-kit-react/views/listRoadmap';
+import classNames from 'classnames';
+import Footer from 'components/Footer/Footer.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import Header from 'components/Header/Header.js';
+import HeaderLinks from 'components/Header/HeaderLinks.js';
+import Parallax from 'components/Parallax/Parallax.js';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useObservable } from 'react-use-observable';
+import { getRoadmapList, list } from 'services/roadmapService';
+
+const useStyles = makeStyles(styles);
+
+const ListRoadmap = () => {
+  const classes = useStyles();
+
+  useEffect(() => {
+    list().subscribe();
+  }, []);
+
+  const [roadropList] = useObservable(() => getRoadmapList(), []);
+
+  if (!roadropList) return null;
+
+  return (
+    <div>
+      <Header
+        color="transparent"
+        brand="RoadRopper"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 200,
+          color: 'white'
+        }}
+      />
+      <Parallax small filter image={require('assets/img/profile-bg.jpg')} />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12}>
+                <div className={classes.name}>
+                  <h3 className={classes.title}>Explore nossos Roadrops</h3>
+                </div>
+              </GridItem>
+              <GridContainer justify="center">
+                {roadropList.map(roadrop => (
+                  <GridItem xs={12} md={10} key={roadrop.id}>
+                    <div className={classes.roadrop}>
+                      <div className="roadrop__header">
+                        <Link className={classes.link} to={`/roadmap/${roadrop.id}`}>
+                          <h2>{roadrop.title}</h2>
+                        </Link>
+                        <span>
+                          {roadrop.category} - {roadrop.target}
+                        </span>
+                      </div>
+                      <div className="roadrop__main">
+                        <div className="roadrop__subject">{roadrop.subject}</div>
+                        <div className="roadrop__content">{roadrop.content}</div>
+                        <div className="roadrop__creator">{roadrop.creator}</div>
+                      </div>
+                      <div className="roadrop__footer">
+                        <span className="roadrop__comment">
+                          <Icon>
+                            <CommentIcon />
+                          </Icon>
+                        </span>
+
+                        <div className="roadrop__honor">
+                          <Icon>
+                            <HonorIcon />
+                          </Icon>
+                          <span>{roadrop.honor}</span>
+                        </div>
+                        <span>Feito: {roadrop.updated_at}</span>
+                        <span>
+                          <Icon>
+                            <Follow />
+                          </Icon>
+                        </span>
+                      </div>
+                    </div>
+                  </GridItem>
+                ))}
+              </GridContainer>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ListRoadmap;
