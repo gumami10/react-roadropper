797be368cff5c7386e393a7e76bfcd05e086851a
Eric Groppe <emmgp@hotmail.com>
2020-07-11 16:49:00 -0300

    make persistant login for users

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,16 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { filter, finalize, switchMap, tap } from 'rxjs/operators';
 import api from 'services/api';
 
 const user$ = new BehaviorSubject(null);
 const loading$ = new BehaviorSubject(false);
+
+const getStatus = () => {
+  return of(window.localStorage.getItem('user')).pipe(
+    filter(data => !!data),
+    tap(data => user$.next(JSON.parse(data)))
+  );
+};
 
 const login = ({ email, password }) => {
   loading$.next(true);
@@ -13,12 +20,12 @@ const login = ({ email, password }) => {
       password: password
     })
     .pipe(
-      tap(data => user$.next(data.user)),
-      tap(data => window.localStorage.setItem('user', JSON.stringify(data.user))),
+      tap(data => user$.next({ ...data.user, ...data.token })),
+      tap(data => window.localStorage.setItem('user', JSON.stringify({ ...data.user, ...data.token }))),
       finalize(() => loading$.next(false))
     );
 };
-//lk
+
 const register = ({ username, email, password, description }) => {
   loading$.next(true);
   return api
@@ -43,10 +50,15 @@ const logout = () => {
   );
 };
 
-const getUser = () => user$.asObservable();
+const getUser = () => {
+  console.log(user$.value);
+  return user$.asObservable();
+};
 
 const loading = () => loading$.asObservable();
 
-const userService = { getUser, login, register, logout, loading };
+const getStaticValue = () => user$.value;
+
+const userService = { getUser, login, register, logout, loading, getStaticValue, getStatus };
 
 export default userService;
