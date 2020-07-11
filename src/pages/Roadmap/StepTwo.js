0e86c3f5bf499cfe5d6357d6aefa6a69a1443cd1
Eric Groppe <emmgp@hotmail.com>
2020-07-11 15:28:17 -0300

    change language for portuguese

--- src/pages/Roadmap/StepTwo.js
+++ src/pages/Roadmap/StepTwo.js
@@ -26,7 +26,9 @@ function StepTwo() {
       'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
     toolbar1:
       'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
-    image_advtab: true
+    image_advtab: true,
+    spellchecker_language: 'pt',
+    language: 'pt_BR'
   };
 
   return (
